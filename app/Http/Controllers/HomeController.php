5f45cb24ffb95fe1c332b75c6ab069e3d66e4710
EkoKun29 <ekoseptyonugroho9@gmail.com>
2025-04-13 20:02:02 +0700

    fetch:api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,7 +50,7 @@ public function index()
     $apis = [
         "1" => [
             "model" => RepeatOrder::class,
-            "url" => "https://sales.dodolanobattani.com/api/1/EDI UST.",
+            "url" => "https://sales.dodolanobattani.com/api/kunjungan/1/EDI%20UST.",
         ],
         "2" => [
             "model" => AgusA::class,
